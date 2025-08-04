4e1fd10810e9b38376cc8b5626d582d93721aff3
Shahbaz Khan <shahbazali.khan@hotmail.com>
2025-08-03 21:53:44 -0400

    education update

--- client/src/components/sections/education.tsx
+++ client/src/components/sections/education.tsx
@@ -29,7 +29,7 @@ export default function Education() {
               <GraduationCap className="w-12 h-12 mx-auto" />
             </div>
             <h3 className="text-2xl font-semibold text-white mb-2">Bachelor of Science</h3>
-            <h4 className="text-xl text-blue-400 mb-2">Computer Science & Digital Marketing</h4>
+            <h4 className="text-xl text-blue-400 mb-2">Computer Science & Software Engineering</h4>
             <p className="text-slate-400 mb-2">National College of Business Administration, Lahore</p>
             <p className="text-slate-500">2011 - 2015</p>
           </GlassCard>
